a53666f1a84fe194bf3b73020216f7772508dd84
niiion <niion.eooo@gmail.com>
2024-01-06 19:59:25 +0900

    [add] Object와 오버라이딩

---
+++ objectclass/Car.java
@@ -0,0 +1,12 @@
+public class Car{
+    String name;
+    int number;
+    
+    //toString을 오버라이드 해 보세요.
+    //return 형식은 아래줄을 참고하세요: 
+    //"name: " + name + ", number: " + number;
+    
+    public String toString() {
+        return "name: " + name + ", number: " + number;
+    }
+}
