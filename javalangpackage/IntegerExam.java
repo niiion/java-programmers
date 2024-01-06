fa4b9d9b168ba16ee49db29dcc695bc2e3391d23
niiion <niion.eooo@gmail.com>
2024-01-06 20:10:07 +0900

    [add] Wrapper class

---
+++ javalangpackage/IntegerExam.java
@@ -0,0 +1,7 @@
+public class IntegerExam{
+    public static void main(String[] args){
+        Integer i1 = 5;
+        int max_int = i1.MAX_VALUE;
+        long i1_long = i1.longValue();
+    }
+}
