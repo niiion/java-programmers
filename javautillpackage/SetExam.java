75a2d300337b994e8103eea531b4dcff8c5f5fd8
niiion <niion.eooo@gmail.com>
2024-01-07 14:31:43 +0900

    [add] Set2

--- javautillpackage/SetExam.java
+++ javautillpackage/SetExam.java
@@ -4,11 +4,12 @@ public class SetExam{
     public static void main(String[] args){
         Set<String> set = new HashSet<String>();
         set.add("a");
-        set.add("a");
         set.add("b");
         
-        System.out.println("set의 내용을 출력합니다.");
-        for(String str : set){
+        Iterator<String> iter = set.iterator();
+        //iter를 이용해서 set의 내용을 출력하세요.
+        while (iter.hasNext()) {
+            String str = iter.next();
             System.out.println(str);
         }
     }
