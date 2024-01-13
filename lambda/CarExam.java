72ac5856c4a4140ecf6751972184b08aa3ecd286
niiion <niion.eooo@gmail.com>
2024-01-13 21:33:51 +0900

    [add] 람다, 내부클래스, 익명클래스3

--- lambda/CarExam.java
+++ lambda/CarExam.java
@@ -1,21 +1,31 @@
 import java.util.*;
 public class CarExam{
     public static void main(String[] args){
-        //Car객체를 만들어서 cars에 넣습니다.
         List<Car> cars = new ArrayList<>();
         cars.add( new Car("작은차",2,800,3) );
         cars.add( new Car("봉고차",12,1500,8) );
         cars.add( new Car("중간차",5,2200,0) );
         cars.add( new Car("비싼차",5,3500,1) );
         
-        printCarCheaperThan(cars, 2000);
+        printCar(cars, new CheckCarForBigAndNotExpensive());
     }
     
-    public static void printCarCheaperThan(List<Car> cars, int price){
+    public static void printCar(List<Car> cars, CheckCar tester){
         for(Car car : cars){
-            if(car.price < price){
+            if (tester.test(car)) {
                 System.out.println(car);
             }
+        }
+    }
+    
+    interface CheckCar{
+        boolean test(Car car);
+    }
+    
+    //내부클래스를 만들어서 사용합니다.
+    static class CheckCarForBigAndNotExpensive implements CheckCar{
+        public boolean test(Car car){
+            return car.capacity >= 4 && car.price < 2500;
         }
     }
 }
